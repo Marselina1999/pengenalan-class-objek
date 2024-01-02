8f4a4cfcd8887c26cb077b8ae42afb7f608b5271
Marselina <128449226+Marselina1999@users.noreply.github.com>
2024-01-02 05:12:46 -0800

    Update 06Pewarisan Main.java

--- 06Pewarisan Main.java
+++ 06Pewarisan Main.java
@@ -1,1 +1,39 @@
+package com.tutorial;
+
+class Hero{
+    //att
+    String nama;
+    float exp;
+    float heallth;
+
+    void display(){
+        System.out.println("Nama Hero: " + this.nama);
+        System.out.println("exp Hero:" + this.exp);
+        System.out.println("health " + this.heallth);
+    }
+
+    }
+
+
+    //subclass
+    class HeroTank extends Hero{
+
+    }
+
+
+
+public class  Main {
+    public static void main(String[] args) {
+        // instansiasi objek
+        HeroTank hero_1 = new HeroTank();
+        hero_1.display();
+
+        HeroTank hero_2 = new HeroTank();
+        hero_2.nama = "biel";
+
+        hero_2.display();
+    }
+}
+
+
 
